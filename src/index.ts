f458ecd90de25d039b6c487925ef7c073ae1e058
Shigma <shigma10826@gmail.com>
2024-01-20 15:07:00 +0800

    feat: migrate to pure esm

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { CompilerOptions } from 'typescript'
 import { readFile } from 'fs/promises'
 import { dirname, resolve } from 'path'
-import { alias, boolean } from './types'
+import { alias, boolean } from './types.js'
 import { fork, ForkOptions } from 'child_process'
 import { createRequire } from 'module'
+import { fileURLToPath } from 'url'
 import json5 from 'json5'
 
 export class TsConfig {
@@ -50,14 +51,14 @@ export namespace TsConfig {
 const cache = new Map<string, TsConfig>()
 
 export async function read(filename: string) {
-  if (cache.has(filename)) return cache.get(filename)
+  if (cache.has(filename)) return cache.get(filename)!
   const source = await readFile(filename, 'utf8')
   const data = json5.parse(source) as TsConfig
   cache.set(filename, data)
   return data
 }
 
-function makeArray(value: string | string[]) {
+function makeArray(value: undefined | string | string[]) {
   return Array.isArray(value) ? value : value ? [value] : []
 }
 
@@ -67,10 +68,10 @@ export async function load(cwd: string, args: string[] = []) {
   const data = await read(filename)
   const queue = makeArray(data.extends)
   outer: while (queue.length) {
-    const final = queue.pop()
+    const final = queue.pop()!
     const paths = final.startsWith('.')
       ? [resolve(dirname(filename), final)]
-      : createRequire(filename).resolve.paths(final).map(path => resolve(path, final))
+      : createRequire(filename).resolve.paths(final)!.map(path => resolve(path, final))
     for (const path of paths) {
       try {
         const name = path.endsWith('.json') ? path : path + '.json'
@@ -86,7 +87,7 @@ export async function load(cwd: string, args: string[] = []) {
         filename = name
         queue.push(...makeArray(parent.extends))
         continue outer
-      } catch (error) {
+      } catch (error: any) {
         if (error.code !== 'ENOENT') throw error
       }
     }
@@ -97,7 +98,8 @@ export async function load(cwd: string, args: string[] = []) {
 }
 
 export async function compile(args: string[], options?: ForkOptions) {
-  const child = fork(require.resolve('typescript/bin/tsc'), args, { stdio: 'inherit', ...options })
+  const path = fileURLToPath(import.meta.resolve('typescript/bin/tsc'))
+  const child = fork(path, args, { stdio: 'inherit', ...options })
   return new Promise<number>((resolve) => {
     child.on('close', resolve)
   })
